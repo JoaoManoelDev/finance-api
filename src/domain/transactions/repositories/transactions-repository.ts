828b93139f2f97b7f51775f3d74c7e0a4514f500
João Manoel <joaomanoeldev@gmail.com>
2025-02-24 22:24:33 -0300

    feat: create owner entity

--- src/domain/transactions/repositories/transactions-repository.ts
+++ src/domain/transactions/repositories/transactions-repository.ts
@@ -2,6 +2,7 @@ import { Transaction } from "@/domain/transactions/entities/transaction"
 
 export interface TransactionsRepository {
   create(transaction: Transaction): Promise<void>
-  findById(id: string): Promise<Transaction | null>
+  findById(transactionId: string): Promise<Transaction | null>
   save(transaction: Transaction): Promise<void>
+  findManyByOwnerId(ownerId: string): Promise<Transaction[]>
 }
