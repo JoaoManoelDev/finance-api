e717b3663dabc7bf3465c412ac591e469ffc3d55
João Manoel <joaomanoeldev@gmail.com>
2025-02-24 22:33:20 -0300

    feat: create in memory owner repository with create owner method

--- src/domain/transactions/repositories/transactions-repository.ts
+++ src/domain/transactions/repositories/transactions-repository.ts
@@ -4,5 +4,5 @@ export interface TransactionsRepository {
   create(transaction: Transaction): Promise<void>
   findById(transactionId: string): Promise<Transaction | null>
   save(transaction: Transaction): Promise<void>
-  findManyByOwnerId(ownerId: string): Promise<Transaction[]>
+  // findManyByOwnerId(ownerId: string): Promise<Transaction[]>
 }
